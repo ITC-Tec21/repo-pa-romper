edcf061e9738afba301ee901ad733b24893d453b
Agustin Pumarejo <aguspumarejo@gmail.com>
2021-04-16 18:54:27 -0500

    add almost working prototype

---
+++ CircuitSandbox1.1/Assets/Scripts/Managers/CircuitLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CircuitLoader : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Sprite[] testSprite;
+    public void Awake()
+    {
+        Debug.Log(Circuit.circuitComponents.Count);
+        for(int x = tilemap.cellBounds.xMin; x < tilemap.cellBounds.xMax; x++)
+        {
+            for(int y = tilemap.cellBounds.yMin; y < tilemap.cellBounds.yMax; y++)
+            {
+                WireTile wire = ScriptableObject.CreateInstance<WireTile>();
+                wire.wireSprites = testSprite;
+                tilemap.SetTile(new Vector3Int(x, y, 0), wire);
+            }
+        }
+        Debug.Log(Circuit.circuitComponents.Count);
+    }
+}
---
+++ CircuitSandbox1.1/Assets/TestScripts/TestTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class TestTile : Tile
+{
+
+    public Sprite testSprite;
+    public override void GetTileData(Vector3Int location, ITilemap tilemap, ref TileData tileData)
+    {
+        
+        bool testTileThere = tilemap.GetTile(location);
+        Debug.Log("There is a tile here (GetTileData): " + location + testTileThere);
+        tileData.sprite = testSprite;
+    }
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        bool testTileThere = tilemap.GetTile(position);
+        Debug.Log("There is a tile here (Refresh): " + position + testTileThere);
+        tilemap.RefreshTile(position);
+    }
+
+    #if UNITY_EDITOR
+    [MenuItem("Assets/Create/TestTile")]
+    public static void CreateTestTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Test Tile", "New Test Tile", "Asset", "Save Test Tile", "Assets");
+        if (path == "")
+            return;
+    AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<TestTile>(), path);
+    }
+    #endif
+}
